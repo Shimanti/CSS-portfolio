40ecdef22ac34a328a4f2165af85373d04e1b820
Shimanti <shim2022@Shimantis-MBP.hsd1.az.comcast.net>
2022-04-03 00:49:07 -0700

    added background-colors to menu buttons

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # CSS-portfolio
 Slowly building portfolio site over time.
-Currently only has a side menu with some basic javascript
+4/1 Currently only has a side menu with some basic javascript
+4/3 Added background colors to side menu buttons
 
 # Inspiration
 https://www.youtube.com/watch?v=xV7S8BhIeBo
--- app.js
+++ app.js
@@ -9,8 +9,21 @@ function pageTransitions() {
         let currentBtn = document.querySelector('.active-btn')
         currentBtn.classList.remove('active-btn')
         btn.classList.add("active-btn")
-        // console.log(btn)
      })
     )
+
+    // change mainscreen
+    allSections.addEventListener('click', e => {
+        // pick id that is the same as the data-id
+        const id = e.target.dataset.id
+        if(id) {
+            // hide other sections
+            sections.forEach(section => section.classList.remove('active'))
+
+            // show section with id 
+            const element = document.getElementById(id)
+            element.classList.add('active')
+        }
+    })
 }
 pageTransitions()
